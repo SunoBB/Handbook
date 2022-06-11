0fc3c493a6b53ea40c3a1a1bd6e21ff16486007c
Nguyen Dinh Bach <94342357+SunoBB@users.noreply.github.com>
2022-06-11 23:03:08 +0700

    Up
    Up

--- Dictionary.py/main.py
+++ Dictionary.py/main.py
@@ -1,20 +1,20 @@
-f=open('hs.inp','r')
-n=int(f.readline())
-m=float(0)
+# B1
+with open('HS.INP', 'r') as f:
+    n = int(f.readline())
+    d = []
+    for i in range(n):
+        d.append(list(map(str, f.readline().strip().split(" "))))
+
+maxx = float(d[0][2])
+indexx = 0
 for i in range(n):
-    b={}
-    a=[j for j in f.readline().split()]
-    b["Họ và tên"]=a[0]
-    b["Ngày sinh"]=a[1]
-    b["Điểm trung bình"]=a[2]
-    if float(b["Điểm trung bình"])>=m:
-        m=float(b["Điểm trung bình"])
-        b1 = b.copy()
+    if maxx < float(d[i][2]):
+        maxx = float(d[i][2])
+        indexx = i
+print(f"Họ và tên: {d[indexx][0]}")
+print(f"Ngày sinh: {d[indexx][1]}")
+print(f"Điểm: {d[indexx][2]}")
 
-for i in b1:
-    print(i,": ",b1[i])
-
-f.close()
 
 
 
